b75ddf12283878974903e1b03ae4b45985821da0
hanyuana <357966741@qq.com>
2023-04-07 22:27:32 +0800

    Receiver 的使用

--- Proxy和Reflect/05_Receiver的使用.js
+++ Proxy和Reflect/05_Receiver的使用.js
@@ -1,22 +1,39 @@
 const obj = {
-    _name:"han",
-    get name(){
+    _name: "Han",
+    get name() {
         return this._name
     },
-    set name(newVal){
+    set name(newVal) {
         this._name = newVal
-    },
+    }
 }
 
-console.log()
-const objProxy = new Proxy(obj,{
-    // receiver 是创建出来的代理对象 就等于ObjProxy
-    get:function(target,key,receiver){
-       return Reflect.get(target,key,receiver)
+
+// obj.name = "韩程远"  //访问的时set方法
+// console.log(obj.name)  //访问的是get方法
+
+
+// 监听obj 
+let objProxy = new Proxy(obj, {
+    get(target, key,receiver) {
+        // receiver 是创建出来的代理对象
+        console.log(`get 方法被访问-----------`,key,receiver)
+
+        console.log(receiver === objProxy)
+
+        // Reflect 在调用get 方法的时候也可以传入 receiver,可以把reciver  传入到get方法 可以改变调用对象里面的get方法里面的this指向
+        return Reflect.get(target, key,receiver)
     },
-    set:function(target,key,newVal,receiver){
-        Reflect.set(target,key,newVal)
+    set(target, key, newVal,receiver) {
+        console.log(`set方法被访问----------`,key)
+        Reflect.set(target, key, newVal,receiver)
     }
 })
-objProxy.name = "hanA";
-console.log(objProxy.name)
+
+// objProxy.name = "kobe";
+// console.log(objProxy.name) // 现在我们想去看objProxy.name 的访问过程是什么样子的呢
+// 1. 因为是objProxy的name，所以会来到proxy的get方法里面
+// 2. get捕获器里面通过Reflect的get捕获器 去拿target 里面的key 
+// 3. 最终访问的还是get name() 方法 
+
+console.log(objProxy.name = "韩");
